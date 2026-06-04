ab0f60a1b964f42e7c85d3892b36a7e0b88455cc
golindian <gasparirrazabal@gmail.com>
2026-06-04 17:56:34 -0300

    feat: agregar configuración de correo electrónico para el formulario de contacto

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -70,3 +70,16 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# Email settings for contact form delivery
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'gasparirrazabal@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+
+# Optional fallback for local development
+if os.environ.get('DJANGO_EMAIL_BACKEND') == 'console':
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
